73eca0ddd1f244c776a431b6de8c70110b4d76b0
markaguirre <mark.s.r.aguirre@accenture.com>
2020-01-21 09:04:28 +0800

    keep login fixed
    Change-Id: Ib2e9434a142649a7fc6fc87888351d6828bdb7d4

--- app/src/main/java/com/thesis/pdm/hallergen/LoginActivity.java
+++ app/src/main/java/com/thesis/pdm/hallergen/LoginActivity.java
@@ -55,7 +55,7 @@ public class LoginActivity extends AppCompatActivity {
         etUsername = findViewById(R.id.etUsername);
         etPassword = findViewById(R.id.etPassword);
 
-//        cbKeepLogin.setVisibility(View.GONE);
+
 //        etUsername.setText("mark");
 //        etPassword.setText("aaaaaaaaa");
 
